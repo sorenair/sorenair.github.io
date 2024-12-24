b88e78ec1aff6dbfac65ea01518cfa7a3697c284
Soren Gabor <soren@Sorens-MacBook-Air-2.local>
2024-12-24 11:06:00 -0600

    added website files

---
+++ navbar.js
@@ -0,0 +1,8 @@
+function showDiv() {
+    if (document.getElementById('nav_mobile_options').style.display == "flex") {
+        document.getElementById('nav_mobile_options').style.display = "none";
+    }
+    else {
+        document.getElementById('nav_mobile_options').style.display = "flex";
+    }
+ }
